ba20c60feadbf4c0aaddc135efc63c8ea3142c24
S. Bozhko <svt.bozhko@gmail.com>
2017-02-14 13:14:00 +0000

    Add simple http endpoint

--- build.sbt
+++ build.sbt
@@ -6,16 +6,12 @@ scalaVersion := "2.12.1"
 
 
 libraryDependencies ++= Seq(
-
   "joda-time" % "joda-time" % "2.9.7",
   "org.json4s" % "json4s-jackson_2.12" % "3.5.0",
-  "org.json4s" % "json4s-ext_2.12" % "3.5.0",
-  "com.typesafe.scala-logging" % "scala-logging_2.12" % "3.5.0",
-  "com.typesafe.akka" % "akka-slf4j_2.12" % "2.4.16",
-  "ch.qos.logback" % "logback-classic" % "1.1.9",
   "org.apache.httpcomponents" % "httpclient" % "4.5.3",
-  "org.apache.httpcomponents" % "fluent-hc" % "4.5.3"
+  "org.apache.httpcomponents" % "fluent-hc" % "4.5.3",
+  "com.typesafe.akka" %% "akka-http-core" % "10.0.1",
+  "com.typesafe.akka" %% "akka-http" % "10.0.1"
 )
-resolvers ++= Seq()
 
 enablePlugins(JavaServerAppPackaging)
--- src/main/scala/ru/devzen/shownotegen/ShownotesGen.scala
+++ src/main/scala/ru/devzen/shownotegen/ShownotesGen.scala
@@ -2,6 +2,14 @@ package ru.devzen.shownotegen
 
 import java.util.regex.Pattern
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Directives.complete
+import akka.http.scaladsl.server.Directives.get
+import akka.http.scaladsl.server.Directives.path
+import akka.http.scaladsl.server.PathMatchers.LongNumber
+import akka.stream.ActorMaterializer
 import org.apache.http.client.fluent.Request
 import org.joda.time.DateTime
 import org.joda.time.Duration
@@ -10,84 +18,78 @@ import org.joda.time.format.PeriodFormatterBuilder
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
+import scala.util.Properties
+
 case class Theme(title: String, urls: List[String], relativeStartReadableTime: String, relativeStartMs: Long)
 
 object ShownotesGen {
 
   def main(args: Array[String]): Unit = {
 
-    val startedRecordingAtMs = 1487069570000l
-
-    val discussedCardsJs = Request.Get(UrlGenerator.getDiscussedListUrl).execute().returnContent().asString()
-    val cards = parse(discussedCardsJs) \ "cards"
-
-    val processed = cards.children.map { card =>
-      val cardId = (card \ "id").values.asInstanceOf[String]
-      val name = (card \ "name").values.asInstanceOf[String]
-      val desc = (card \ "desc").values.asInstanceOf[String]
-
-      val urls = extractUrls(desc)
-
-      val cardInfoJs = Request.Get(UrlGenerator.getCardInfoUrl(cardId)).execute().returnContent().asString()
-
-      val dragAndDropEvents = parse(cardInfoJs).children.filter { event =>
-        val eventType = (event \ "type").values.asInstanceOf[String]
-        "updateCard" == eventType
-      }
-
-      val topicStartedAtMs = dragAndDropEvents.flatMap { event =>
-        val listAfterId = (event \ "data" \ "listAfter" \ "id").values.asInstanceOf[String]
-        val listBeforeId = (event \ "data" \ "listBefore" \ "id").values.asInstanceOf[String]
-
-        if (Constants.ToDiscussCurrentEpisodeListId == listBeforeId && Constants.InDiscussionListId == listAfterId) {
-          val dateString = (event \ "date").values.asInstanceOf[String]
-          Some(DateTime.parse(dateString).getMillis)
-        } else None
-      }
-
-      if (topicStartedAtMs.size != 1) {
-        println("WARN - Unexpected movements of a card from 'to discuss' to 'in discussion'")
-      }
-
-      val period: Period = new Duration(startedRecordingAtMs, topicStartedAtMs.max).toPeriod
-      val hoursMinutesAndSeconds = new PeriodFormatterBuilder()
-        .printZeroAlways()
-        .minimumPrintedDigits(2)
-        .appendHours()
-        .appendSeparator(":")
-        .appendMinutes()
-        .appendSeparator(":")
-        .appendSeconds()
-        .toFormatter
-      val relativeStartStr = hoursMinutesAndSeconds.print(period)
-
-      Theme(name, urls, relativeStartStr, topicStartedAtMs.max)
-    }.sortWith((t1, t2) => t1.relativeStartMs <= t2.relativeStartMs)
-
-    println("<ul>")
-    processed.foreach { theme =>
-      print(s"<li>[${theme.relativeStartReadableTime}] ")
-
-      theme.urls.size match {
-        case 0 =>
-          println(s"${theme.title}</li>")
-        case 1 =>
-          println(s"""<a href="${theme.urls.head}">${theme.title}</a></li>""")
-        case _ =>
-          println(theme.title)
-          println("<ul>")
-          theme.urls.foreach { url =>
-            println(s"""<li><a href="$url">$url</a></li>""")
-          }
-          println("</ul>")
-          println("</li>")
-      }
-    }
-    println("</ul>")
-
+    implicit val system = ActorSystem("shownotegenerator")
+    implicit val materializer = ActorMaterializer()
+    val routes = getRoute
+    Http().bindAndHandle(routes, "0.0.0.0", Properties.envOrElse("PORT", "9025").toInt)
+    println(s"Server online")
   }
 
-  def extractUrls(text: String): List[String] = {
+  private def getRoute = {
+    pathPrefix("generate") {
+      (get & path(LongNumber)) { startedRecordingAtMs =>
+        complete {
+          val discussedCardsJs = Request.Get(UrlGenerator.getDiscussedListUrl).execute().returnContent().asString()
+          val cards = parse(discussedCardsJs) \ "cards"
+
+          val processedThemes = cards.children.map { card =>
+            val cardId = (card \ "id").values.asInstanceOf[String]
+            val name = (card \ "name").values.asInstanceOf[String]
+            val desc = (card \ "desc").values.asInstanceOf[String]
+
+            val urls = extractUrls(desc)
+
+            val cardInfoJs = Request.Get(UrlGenerator.getCardInfoUrl(cardId)).execute().returnContent().asString()
+
+            val dragAndDropEvents = parse(cardInfoJs).children.filter { event =>
+              val eventType = (event \ "type").values.asInstanceOf[String]
+              "updateCard" == eventType
+            }
+
+            val topicStartedAtMs = dragAndDropEvents.flatMap { event =>
+              val listAfterId = (event \ "data" \ "listAfter" \ "id").values.asInstanceOf[String]
+              val listBeforeId = (event \ "data" \ "listBefore" \ "id").values.asInstanceOf[String]
+
+              if (Constants.ToDiscussCurrentEpisodeListId == listBeforeId && Constants.InDiscussionListId == listAfterId) {
+                val dateString = (event \ "date").values.asInstanceOf[String]
+                Some(DateTime.parse(dateString).getMillis)
+              } else None
+            }
+
+            if (topicStartedAtMs.size != 1) {
+              println("WARN - Unexpected movements of a card from 'to discuss' to 'in discussion'")
+            }
+
+            val period: Period = new Duration(startedRecordingAtMs, topicStartedAtMs.max).toPeriod
+            val hoursMinutesAndSeconds = new PeriodFormatterBuilder()
+              .printZeroAlways()
+              .minimumPrintedDigits(2)
+              .appendHours()
+              .appendSeparator(":")
+              .appendMinutes()
+              .appendSeparator(":")
+              .appendSeconds()
+              .toFormatter
+            val relativeStartStr = hoursMinutesAndSeconds.print(period)
+
+            Theme(name, urls, relativeStartStr, topicStartedAtMs.max)
+          }.sortWith((t1, t2) => t1.relativeStartMs <= t2.relativeStartMs)
+
+          generateHtml(processedThemes)
+        }
+      }
+    }
+  }
+
+  private def extractUrls(text: String): List[String] = {
     val containedUrls = new scala.collection.mutable.ArrayBuffer[String]()
     val urlRegex = "((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)"
     val pattern = Pattern.compile(urlRegex, Pattern.CASE_INSENSITIVE)
@@ -100,6 +102,30 @@ object ShownotesGen {
     containedUrls.toList
   }
 
+  private def generateHtml(processed: List[Theme]): String = {
+    val response = StringBuilder.newBuilder
+    response.append("<ul>\n")
+    processed.foreach { theme =>
+      response.append(s"<li>[${theme.relativeStartReadableTime}] ")
+
+      theme.urls.size match {
+        case 0 =>
+          response.append(s"${theme.title}</li>\n")
+        case 1 =>
+          response.append(s"""<a href="${theme.urls.head}">${theme.title}</a></li>\n""")
+        case _ =>
+          response.append(theme.title + "\n")
+          response.append("<ul>\n")
+          theme.urls.foreach { url =>
+            response.append(s"""<li><a href="$url">$url</a></li>\n""")
+          }
+          response.append("</ul>\n")
+          response.append("</li>\n")
+      }
+    }
+    response.append("</ul>\n")
+    response.toString
+  }
 }
 
 object Constants {
