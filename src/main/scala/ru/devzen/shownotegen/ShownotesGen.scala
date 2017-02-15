d8c7b1c63343578c954e30d1da3e0199d834803b
S. Bozhko <svt.bozhko@gmail.com>
2017-02-15 12:32:01 +0000

    Post messages to Gitter

--- src/main/scala/ru/devzen/shownotegen/ShownotesGen.scala
+++ src/main/scala/ru/devzen/shownotegen/ShownotesGen.scala
@@ -11,6 +11,7 @@ import akka.http.scaladsl.server.Directives.get
 import akka.http.scaladsl.server.Directives.path
 import akka.stream.ActorMaterializer
 import org.apache.http.client.fluent.Request
+import org.apache.http.entity.ContentType
 import org.joda.time.DateTime
 import org.joda.time.Duration
 import org.joda.time.Period
@@ -28,7 +29,7 @@ object ShownotesGen {
 
     implicit val system = ActorSystem("shownotegenerator")
     implicit val materializer = ActorMaterializer()
-    val routes = getRoute ~ trellohook
+    val routes = getRoute ~ trelloHook
     Http().bindAndHandle(routes, "0.0.0.0", Properties.envOrElse("PORT", "9025").toInt)
     println(s"Server online")
   }
@@ -58,7 +59,7 @@ object ShownotesGen {
               val listAfterId = (event \ "data" \ "listAfter" \ "id").values.asInstanceOf[String]
               val listBeforeId = (event \ "data" \ "listBefore" \ "id").values.asInstanceOf[String]
 
-              if (Constants.ToDiscussCurrentEpisodeListId == listBeforeId && Constants.InDiscussionListId == listAfterId) {
+              if (Constants.TrelloToDiscussCurrentEpisodeListId == listBeforeId && Constants.TrelloInDiscussionListId == listAfterId) {
                 val dateString = (event \ "date").values.asInstanceOf[String]
                 Some(DateTime.parse(dateString).getMillis)
               } else None
@@ -97,25 +98,6 @@ object ShownotesGen {
     }
   }
 
-  private def trellohook = {
-    path("trellohook" ~ Slash.?) {
-      post {
-        entity(as[String]) { json =>
-          complete {
-            println(json)
-            StatusCodes.OK
-          }
-        }
-      } ~
-        get {
-          complete {
-            StatusCodes.OK
-          }
-        }
-    }
-  }
-
-
   private def extractUrls(text: String): List[String] = {
     val containedUrls = new scala.collection.mutable.ArrayBuffer[String]()
     val urlRegex = "((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)"
@@ -153,35 +135,88 @@ object ShownotesGen {
     response.append("</ul>\n")
     response.toString
   }
+
+  private def trelloHook = {
+    path("trellohook" ~ Slash.?) {
+      post {
+        entity(as[String]) { json =>
+          val event = parse(json)
+          val eventType = (event \ "type").values.asInstanceOf[String]
+          if ("updateCard" == eventType) {
+            val listBeforeId = (event \ "action" \ "data" \ "listBefore" \ "id").values.asInstanceOf[String]
+            val listAfterId = (event \ "action" \ "data" \ "listAfter" \ "id").values.asInstanceOf[String]
+
+            if (Constants.TrelloToDiscussCurrentEpisodeListId == listBeforeId && Constants.TrelloInDiscussionListId == listAfterId) {
+              val title = (event \ "action" \ "data" \ "card" \ "name").values.asInstanceOf[String]
+              val cardId = (event \ "action" \ "data" \ "card" \ "id").values.asInstanceOf[String]
+
+              val cardInfoJs = Request.Get(UrlGenerator.getCardInfoUrl(cardId)).execute().returnContent().asString()
+              val parsedCardDesc = (parse(cardInfoJs) \ "desc").values.asInstanceOf[String]
+              val urls = extractUrls(parsedCardDesc)
+
+              Request
+                .Post(UrlGenerator.postMessageToGitterChannel)
+                .addHeader("Authorization", s"Bearer ${Config.GitterAccessToken}")
+                .bodyString(
+                  s"""
+                     |{
+                     |  "text" : "$title\n${urls.mkString("\n")}"                      |
+                     |}
+                    """.stripMargin, ContentType.APPLICATION_JSON)
+                .execute().discardContent()
+            }
+          }
+
+          complete {
+            println("Sent message to Gitter")
+            StatusCodes.OK
+          }
+        }
+      } ~
+        get {
+          complete {
+            StatusCodes.OK
+          }
+        }
+    }
+  }
 }
 
 object Constants {
-  val DiscussedListId = "58a25bbd8d9dd346cdfbc209"
-  val ToDiscussCurrentEpisodeListId = "58a25bb3ee8b2c466ea64742"
-  val InDiscussionListId = "58a25bb92cab934b5ee79fe8"
-  val RecordingStartedCardId = "58a311747827da4eccd243e1"
+  val TrelloDiscussedListId = "58a25bbd8d9dd346cdfbc209"
+  val TrelloToDiscussCurrentEpisodeListId = "58a25bb3ee8b2c466ea64742"
+  val TrelloInDiscussionListId = "58a25bb92cab934b5ee79fe8"
+  val TrelloRecordingStartedCardId = "58a311747827da4eccd243e1"
+
+  val GitterDevzenRoomId = "577e2d7fc2f0db084a21df2b"
 }
 
 object UrlGenerator {
 
   def getDiscussedListUrl: String = {
-    s"https://api.trello.com/1/lists/${Constants.DiscussedListId}?" +
-      s"fields=name&cards=open&card_fields=name,desc&key=${Config.ApiKey}&token=${Config.ReadToken}"
+    s"https://api.trello.com/1/lists/${Constants.TrelloDiscussedListId}?" +
+      s"fields=name&cards=open&card_fields=name,desc&key=${Config.TrelloApplicationKey}&token=${Config.TrelloReadToken}"
   }
 
   def getCardActionsUrl(id: String): String = {
-    s"https://api.trello.com/1/cards/$id/actions?key=${Config.ApiKey}&token=${Config.ReadToken}"
+    s"https://api.trello.com/1/cards/$id/actions?key=${Config.TrelloApplicationKey}&token=${Config.TrelloReadToken}"
   }
 
   def getCardInfoUrl(id: String): String = {
-    s"https://api.trello.com/1/cards/$id?key=${Config.ApiKey}&token=${Config.ReadToken}"
+    s"https://api.trello.com/1/cards/$id?key=${Config.TrelloApplicationKey}&token=${Config.TrelloReadToken}"
+  }
+
+  def postMessageToGitterChannel: String = {
+    s"https://api.gitter.im/v1/rooms/${Constants.GitterDevzenRoomId}/chatMessages"
   }
 
 }
 
 object Config {
 
-  val ApiKey = "a4a7b11c520772e330256e6c9fe0274b"
-  val ReadToken = "737c3884da123617b34e4e749c7c326e59fb8ec1887a7ef6c420dacdd82734a2"
+  val TrelloApplicationKey = "a4a7b11c520772e330256e6c9fe0274b"
+  val TrelloReadToken = "737c3884da123617b34e4e749c7c326e59fb8ec1887a7ef6c420dacdd82734a2"
+
+  val GitterAccessToken = "461c74097fd21822e904ed80f35f4867df5a4c0a"
 
 }
